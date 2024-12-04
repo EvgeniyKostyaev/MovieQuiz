99690445bdcf1ec71fe8e4362efe03131df2d83d
Evgeniy <evgeniy.kostyaev@infobric.com>
2024-12-05 00:43:49 +0400

    Created mack data and connected interface elements

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,11 +1,78 @@
 import UIKit
 
 final class MovieQuizViewController: UIViewController {
+    
+    @IBOutlet private var imageView: UIImageView!
+    @IBOutlet private var textLabel: UILabel!
+    @IBOutlet private var counterLabel: UILabel!
+    
+    private var questions: [QuizQuestion] = []
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        questions = createQuizQuestions()
+        
+        
 
     }
+    
+    // MARK: - Helper methods
+    private func createQuizQuestions() -> [QuizQuestion] {
+        var quizQuestions: [QuizQuestion] = []
+        
+        let godfatherQuestion = QuizQuestion.init(image: "The Godfather", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true)
+        quizQuestions.append(godfatherQuestion)
+        
+        let darkKnightQuestion = QuizQuestion.init(image: "The Dark Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true)
+        quizQuestions.append(darkKnightQuestion)
+        
+        let killBillQuestion = QuizQuestion.init(image: "Kill Bill", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true)
+        quizQuestions.append(killBillQuestion)
+        
+        let avengersQuestion = QuizQuestion.init(image: "The Avengers", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true)
+        quizQuestions.append(avengersQuestion)
+        
+        let deadpoolQuestion = QuizQuestion.init(image: "Deadpool", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true)
+        quizQuestions.append(deadpoolQuestion)
+        
+        let greenKnightQuestion = QuizQuestion.init(image: "The Green Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true)
+        quizQuestions.append(greenKnightQuestion)
+        
+        let oldQuestion = QuizQuestion.init(image: "Old", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false)
+        quizQuestions.append(oldQuestion)
+        
+        let iceAgedQuestion = QuizQuestion.init(image: "The Ice Age Adventures of Buck Wild", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false)
+        quizQuestions.append(iceAgedQuestion)
+        
+        let teslaQuestion = QuizQuestion.init(image: "Tesla", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false)
+        quizQuestions.append(teslaQuestion)
+        
+        let vivariumQuestion = QuizQuestion.init(image: "Vivarium", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false)
+        quizQuestions.append(vivariumQuestion)
+        
+        return quizQuestions
+    }
+    
+    // MARK: - Action methods
+    @IBAction private func yesButtonClicked(_ sender: UIButton) {
+
+    }
+
+    @IBAction private func noButtonClicked(_ sender: UIButton) {
+
+    }
+}
+
+struct QuizQuestion {
+  // строка с названием фильма,
+  // совпадает с названием картинки афиши фильма в Assets
+  let image: String
+  // строка с вопросом о рейтинге фильма
+  let text: String
+  // булевое значение (true, false), правильный ответ на вопрос
+  let correctAnswer: Bool
 }
 
 /*
