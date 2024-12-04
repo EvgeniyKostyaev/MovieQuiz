c74a3c048b01103b07d53a89ff3cf971d040fdde
Evgeniy <evgeniy.kostyaev@infobric.com>
2024-12-05 01:55:36 +0400

    Created "showNextQuestionOrResults" method

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -80,6 +80,23 @@ final class MovieQuizViewController: UIViewController {
         imageView.layer.borderWidth = 8
         imageView.layer.cornerRadius = 20
         imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+           self.showNextQuestionOrResults()
+        }
+    }
+    
+    private func showNextQuestionOrResults() {
+        if currentQuestionIndex == questions.count - 1 {
+            
+        } else {
+            currentQuestionIndex += 1
+            
+            let nextQuestion = questions[currentQuestionIndex]
+            let nextStep = convert(model: nextQuestion)
+            
+            show(quiz: nextStep)
+        }
     }
     
     // MARK: - Action methods
