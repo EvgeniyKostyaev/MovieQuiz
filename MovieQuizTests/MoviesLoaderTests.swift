2e6e4aafc09d209f44cbde5813e94b6a73bbda1b
Evgeniy Kostyaev <evgeniy.kostyaev@infobric.com>
2025-01-13 18:54:59 +0400

    Implemented "MoviesLoaderTests" unit test

---
+++ MovieQuizTests/MoviesLoaderTests.swift
@@ -0,0 +1,62 @@
+//
+//  MoviesLoaderTests.swift
+//  MovieQuizTests
+//
+//  Created by Evgeniy Kostyaev on 13.01.2025.
+//
+
+import XCTest
+@testable import MovieQuiz
+
+class MoviesLoaderTests: XCTestCase {
+    
+    func testSuccessLoading() throws {
+        // Given
+        let stubNetworkClient = StubNetworkClient(emulateError: false)
+        var loader = MoviesLoader()
+        loader.networkClient = stubNetworkClient
+        
+        // When
+        let expectation = expectation(description: "Loading expectation")
+        
+        loader.loadMovies { result in
+                // Then
+                switch result {
+                case .success(let movies):
+                    XCTAssertEqual(movies.items.count, 2)
+                    
+                    expectation.fulfill()
+                case .failure(_):
+                    XCTFail("Unexpected failure")
+                }
+            }
+           
+           waitForExpectations(timeout: 1)
+        
+    }
+    
+    func testFailureLoading() throws {
+        // Given
+        let stubNetworkClient = StubNetworkClient(emulateError: true)
+        var loader = MoviesLoader()
+        loader.networkClient = stubNetworkClient
+        
+        // When
+        let expectation = expectation(description: "Loading expectation")
+        
+        loader.loadMovies { result in
+                // Then
+                switch result {
+                case .success(_):
+                    XCTFail("Unexpected failure")
+                    
+                case .failure(let error):
+                    XCTAssertNotNil(error)
+                    
+                    expectation.fulfill()
+                }
+            }
+           
+           waitForExpectations(timeout: 1)
+    }
+}
